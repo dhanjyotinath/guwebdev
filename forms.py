880de66e113e224aa543bed1e6bb7050fc84d2ef
dhanjyotinath <142841972+dhanjyotinath@users.noreply.github.com>
2026-05-02 22:58:00 +0530

    forms.py

--- forms.py
+++ forms.py
@@ -1,20 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, IntegerField, SelectField
-from wtforms.validators import DataRequired, NumberRange
-from wtforms import RadioField, SubmitField
-from wtforms import DateField
+from wtforms import StringField, EmailField, IntegerField, RadioField, SubmitField, DateField, FileField
+from wtforms.validators import DataRequired, NumberRange, Regexp
+from flask_wtf.file import FileAllowed
 
 class TestForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-   
+
+    email = EmailField('Email', validators=[
+        DataRequired(),
+        Regexp(r'^[a-zA-Z0-9._%+-]+@gauhati\.ac\.in$', message="Only @gauhati.ac.in email allowed")
+    ])
+
     age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=1)])
+
     gender = RadioField('Gender', choices=[
         ('M', 'Male'),
         ('F', 'Female'),
         ('O', 'Other')
-    ])
+    ], validators=[DataRequired()])
+
     birth_date = DateField('Birth Date', format='%Y-%m-%d', validators=[DataRequired()])
-     
-     
-     
+
+    file = FileField('Upload File', validators=[
+        DataRequired(),
+        FileAllowed(['pdf', 'jpg', 'jpeg'], 'Only PDF and JPG allowed!')
+    ])
+
+    submit = SubmitField('Submit')
